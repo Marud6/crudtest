b66a2acae1aa34428f9014ba5ce248ec4f6851eb
MerekKukla <mkukla.cz@gmail.com>
2023-12-01 17:01:05 +0100

    zmena

--- delete.php
+++ delete.php
@@ -1,9 +1,9 @@
 <?php
 $id= $_GET["id"];
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname=("bloky");
+$dbhost = "mysql.houkal.cz";
+$dbuser = "mojedb-houkal";
+$dbpass = "Marekdb2007";
+$dbname=("mojedb-houkal");
 $mysql= new mysqli($dbhost,$dbuser,$dbpass, $dbname);
 if($mysql->connect_error) {
    echo 'connection failed';
