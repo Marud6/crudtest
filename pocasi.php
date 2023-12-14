5174abaeed9e2208f85a49da0b1803f68054ff6c
MerekKukla <mkukla.cz@gmail.com>
2023-12-14 16:11:18 +0100

    pocasí extension

--- pocasi.php
+++ pocasi.php
@@ -4,6 +4,14 @@ include"./compheader.php";
 echo createHeader();
 ?>
 <body>
-    <h1 >KOUKNI SE Z OKNAaaaaa</h1>
+    <h1 style="
+    margin: auto;
+    width: 640px; 
+    padding: 50px;
+    font-family: 'Lexend Deca', sans-serif; 
+    text-align: center;
+
+
+">KOUKNI SE Z OKNAaaaaa</h1>
 </body>
 </html>
