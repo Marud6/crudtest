a78bf9856056879b039b7050e8689d3c828c4c07
MerekKukla <mkukla.cz@gmail.com>
2023-12-19 13:32:40 +0100

    pocasi api

--- pocasi.php
+++ pocasi.php
@@ -10,7 +10,6 @@ include"./compheader.php";
 
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-echo"worked";
 $user_ip = getenv('REMOTE_ADDR');
 $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
 $country = $geo["geoplugin_countryName"];
@@ -21,7 +20,7 @@ $city = $geo["geoplugin_city"];
         $city=htmlspecialchars($_POST["cityy"]);}
     $url='http://api.openweathermap.org/data/2.5/weather?q='.$city.'&appid='.$key;
     $weather=json_decode(file_get_contents($url), true);
-   // echo"<pre>";
+   // echo"<pre>";  
     //print_r($weather);
     $temperature=$weather["main"]["temp"]-273.15;//kelviny na C
 //echo round($temperature);
@@ -63,7 +62,6 @@ echo createHeader();
 <button class="btn btn-success" type="submit" >Submit</button>
 
 </form>
-<button class="btn btn-success"  id="btn">Show current location weather</button>
 
 
 </body>
