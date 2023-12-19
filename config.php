2d9a17459406749938e4a8321ee74d3a3217b350
Marud6 <107268919+Marud6@users.noreply.github.com>
2023-12-19 14:39:44 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_SESSION["host"]="mysql.houkal.cz";
-$_SESSION["usr"]="mojedb-houkal";
-$_SESSION["pas"]="Marekdb2007";
-$_SESSION["db"]="mojedb-houkal";
-$_SESSION["api_key"]="1f60fbb1bd7ffac98f7968dc3e01a65d";
-
-?>
